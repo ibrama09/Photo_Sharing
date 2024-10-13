175bc1e95ad5ff333c71b36a35d3b082781ca93e
ibrama09 <141384467+ibrama09@users.noreply.github.com>
2024-10-13 09:54:02 +0700

    Add files via upload

---
+++ routeList.jsx
@@ -0,0 +1,27 @@
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import UserDetail from "../pages/UserDetail";
+import ProtectedRoute from "./ProtectedRoute";
+
+export const routeList = [
+    {
+        path: "/",
+        element: (
+            <ProtectedRoute>
+                <Home />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/user-detail/:id",
+        element: (
+            <ProtectedRoute>
+                <UserDetail />,
+            </ProtectedRoute>
+        ),
+    },
+];
